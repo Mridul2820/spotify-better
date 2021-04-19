1f133f939e378c4dfb7fd87e8fad9b404ddaca6c
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-19 19:19:33 +0530

    Update useAuth.js

--- clint/src/hooks/useAuth.js
+++ clint/src/hooks/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
     useEffect(() => {
         axios
-        .post("http://localhost:3001/login", {
+        .post("https://spotify-react-express.herokuapp.com/login", {
             code,
         })
         .then(res => {
@@ -26,7 +26,7 @@ export default function useAuth(code) {
         if (!refreshToken || !expiresIn) return
         const interval = setInterval(() => {
         axios
-            .post("http://localhost:3001/refresh", {
+            .post("https://spotify-react-express.herokuapp.com/refresh", {
                 refreshToken,
             })
             .then(res => {
